399b15ad0c3bc38656a4e27872596a35b5ce0548
Recognized <vlad9931@gmail.com>
2018-12-07 23:30:10 +0300

    Update

--- console/src/main/kotlin/vladsaif/gitcards/Main.kt
+++ console/src/main/kotlin/vladsaif/gitcards/Main.kt
@@ -4,36 +4,37 @@ package vladsaif.gitcards
 
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
+import java.io.InputStreamReader
+import java.nio.charset.Charset
 import java.nio.file.Files
 import java.nio.file.Paths
 
 fun main(args: Array<String>) {
-  System.setIn(Files.newInputStream(Paths.get("words.txt")))
-  try {
-    val dataFile = if (args.isNotEmpty()) Paths.get(args.first()) else Paths.get("cards.json")
-    val existingCards = mutableSetOf<Card>()
-    if (Files.exists(dataFile)) {
-      existingCards.addAll(dataFile.toFile().bufferedReader().use { reader ->
-        Gson().fromJson<List<Card.Descriptor>>(reader, (object : TypeToken<List<Card.Descriptor>>() {}).type)
-          .map { it.toCard() }
-      })
-    }
-    var maxId = existingCards.maxBy { it.id }?.id ?: 0
-    while (true) {
-      try {
-        val line = readLine() ?: break
-        if (line.toLowerCase() == ":q") break
-        val (a, b) = line.split("=")
-        if (a.isEmpty() || b.isEmpty()) throw Exception()
-        existingCards.add(TextCard(++maxId, a.trim(), b.trim()))
-      } catch (ex: Throwable) {
-        println("Oops...")
+  InputStreamReader(Files.newInputStream(Paths.get("words.txt")), Charset.forName("UTF-8")).buffered().use {
+    try {
+      val dataFile = if (args.isNotEmpty()) Paths.get(args.first()) else Paths.get("cards.json")
+      val existingCards = mutableSetOf<Card>()
+      var maxId = 0
+      while (true) {
+        try {
+          val line = it.readLine() ?: break
+          if (line.trim() == "*") {
+            maxId++
+            continue
+          }
+          if (line.toLowerCase() == ":q") break
+          val (a, b) = line.split("=")
+          if (a.isEmpty() || b.isEmpty()) throw Exception()
+          existingCards.add(TextCard(++maxId, a.trim(), b.trim()))
+        } catch (ex: Throwable) {
+          println("Oops...")
+        }
       }
+      dataFile.toFile().outputStream().bufferedWriter().use { writer ->
+        writer.append(Gson().toJson(existingCards.map { it.toDescriptor() }))
+      }
+    } catch (ex: Throwable) {
+      println(ex.message)
     }
-    dataFile.toFile().outputStream().bufferedWriter().use { writer ->
-      writer.append(Gson().toJson(existingCards.map { it.toDescriptor() }))
-    }
-  } catch (ex: Throwable) {
-    println(ex.message)
   }
 }
